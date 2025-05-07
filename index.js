b183338d68e7701bed605a0fbc29ccf8637f4668
Gustavo Vieira <gustavoabc2005@hotmail.com>
2025-05-06 23:27:38 -0300

    resolvendo desafios

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
 let iniciar = prompt("Deseja iniciar o programa? s/n")
 
 if (iniciar == "s") {
-    alert("Programa encerrado!")
-} else {
     alert("Programa iniciado!")
     let opcao = prompt("Escolha uma ação a ser feita -> 1 - Mensagem motivacional; 2 - Mensagem desmotivacional; 3 - nada")
-    switch (iniciar) {
-        case 1:
-            alert("Você NÃO VAI conseguir, desista!")
-        case 2:
+    switch (opcao) {
+        case "1":
             alert("Continue tentando, você VAI conseguir!")
-        case 3:
-            alert("Opção inválida selecione um número de 1 a 3.")
-        default:
+            break;
+        case "2":
+            alert("Você NÃO VAI conseguir, desista!")
+            break;
+        case "3":
             alert("...")
+            break;
+            default:
+            alert("Opção inválida selecione um número de 1 a 3.")
+            break;
     }
+} else {
+    alert("Programa encerrado!")
 }
